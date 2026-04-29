a6c866ed59af3995dccf98b00c5df01b634ca65a
Adnan Muradbegović <adnan.muradbegovic@hotmail.com>
2026-04-30 00:04:34 +0200

    Implementiran model Proizvod sa usko vezanim modelima

---
+++ Implementacija/SakuraWeb/Models/Benefit.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SakuraWeb.Models
+{
+    public class Benefit
+    {
+        [Key]
+        public int id { get; set; }
+        public required string opis { get; set; }
+        public required string URLSlike { get; set; }
+    }
+}
---
+++ Implementacija/SakuraWeb/Models/ProizvodBenefit.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SakuraWeb.Models
+{
+    public class ProizvodBenefit
+    {
+        [Key]
+        public int id { get; set; }
+
+        [ForeignKey("Proizvod")]
+        public int proizvodId { get; set; }
+
+        [ForeignKey("Benefit")]
+        public int benefitId { get; set; }
+
+        public Proizvod Proizvod { get; set; }
+        public Benefit Benefit { get; set; }
+    }
+}
